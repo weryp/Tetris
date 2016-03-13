460306c7ae05547a2544a69e7e308700a502ef14
paul wery <paul.wery@epitech.eu>
2016-03-13 23:10:50 +0100

    h

--- srcs/game/main.c
+++ srcs/game/main.c
@@ -5,13 +5,12 @@
 ** Login   <wery_p@epitech.net>
 **
 ** Started on  Sun Feb 28 07:09:19 2016 Paul Wery
-** Last update Sun Mar 13 22:45:34 2016 Paul Wery
+** Last update Sun Mar 13 23:08:43 2016 Paul Wery
 */
 
 #include <ncurses.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "print_param.h"
 #include "tetris.h"
 
 void	aff_map(char **map, t_events *ev)
@@ -29,7 +28,7 @@ void	aff_map(char **map, t_events *ev)
 	  mvprintw((LINES / 2) - (ev->lines / 2 + 1) + n,
 		   (COLS / 2) - ((ev->cols + 2 + 26 +
 				  ev->m_w) / 2 - 22)
-		   + i, &(map[n][i]));
+		   + i, &map[n][i]);
 	  i += 1;
 	}
       n += 1;
@@ -51,16 +50,15 @@ char	**create_aff_map(int n, int i, t_events *ev)
       while (i < (ev->cols + 2))
 	{
 	  if (n == 0 || n == (ev->lines + 1))
-	    map[n][i] = '-';
+	    map[n][i++] = '-';
 	  else if (i == 0 || i == (ev->cols + 1))
-	    map[n][i] = '|';
+	    map[n][i++] = '|';
 	  else
-	    map[n][i] = ' ';
-	  i += 1;
+	    map[n][i++] = ' ';
 	}
       map[n++][i] = '\0';
     }
-  map[n] = NULL;
+    map[n] = NULL;
   return (map);
 }
 
@@ -82,15 +80,15 @@ char	**ini_tetris(t_events *ev)
   return (map);
 }
 
-void		start_ncurses(t_tetris *tet, t_events *ev,
-			      SCREEN *scr, int end)
+void	start_ncurses(t_tetris *tet, t_events *ev,
+		      SCREEN *scr, int end)
 {
-  char		**map;
-  char		*title;
+  char	**map;
+  char	*title;
 
   title = get_title("datas/title.txt");
-  if ((map = ini_tetris(ev)) != NULL && (ev->nb_tet > 0) &&
-      (ev->cols >= ev->m_w && ev->lines >= ev->m_h))
+  if ((map = ini_tetris(ev)) != NULL && ev->nb_tet > 0 &&
+      ev->cols >= ev->m_w && ev->lines >= ev->m_h)
     while (cstr(ev->key, ev->key_quit) == 0)
       {
 	copstr(ev->key, "\0", 0);
@@ -116,8 +114,11 @@ int	main(int ac, char **av)
   t_ini	i;
 
   help(ac, av, 0);
-  if (((i.tet = create_list()) == NULL) ||
-      (ini_term(&i.num) == -1) || (load_tetriminos(i.tet) != 0))
+  if ((i.tet = create_list()) == NULL)
+    return (0);
+  if (ini_term(&i.num) == -1)
+    return (0);
+  if (load_tetriminos(i.tet) != 0)
     return (0);
   ini_events(&i.ev, i.tet);
   ini_game(&i.ev, &i.num);
@@ -125,9 +126,10 @@ int	main(int ac, char **av)
     error_params(ac, av);
   start_debug(&i.ev, i.tet);
   delete_errors_elems(i.tet);
-  if (((i.scr = newterm(NULL, stdout, stdin)) == NULL) ||
-      (error_size(&i.ev, i.scr, i.tet)) ||
-      (ini_end_read(0) == -1))
+  if ((i.scr = newterm(NULL, stdout, stdin)) == NULL)
+    return (0);
+  error_size(&i.ev, i.scr, i.tet);
+  if (ini_end_read(0) == -1)
     return (0);
   start_ncurses(i.tet, &i.ev, i.scr, 0);
   ini_end_read(1);
